0fbea92a007cb3f871575d798082d25da45652fb
MileKat <arthur.barthelme78@gmail.com>
2020-10-26 23:36:40 +0100

    - update gitignore - move api to back_end directory

---
+++ back_end/apiTimeManager/lib/apiTime/worktime/workingtime.ex
@@ -0,0 +1,19 @@
+defmodule Todolist.Worktime.Workingtime do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "workingtimes" do
+    field :end, :naive_datetime
+    field :start, :naive_datetime
+    belongs_to :user, Todolist.Accounts.User
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(workingtime, attrs) do
+    workingtime
+    |> cast(attrs, [:start, :end, :user_id])
+    |> validate_required([:start, :end, :user_id])
+  end
+end
---
+++ back_end/apiTimeManager/lib/apiTime_web/views/user_view.ex
@@ -0,0 +1,20 @@
+defmodule TodolistWeb.UserView do
+  use TodolistWeb, :view
+  alias TodolistWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      id: user.id,
+      username: user.username,
+      email: user.email
+    }
+  end
+end

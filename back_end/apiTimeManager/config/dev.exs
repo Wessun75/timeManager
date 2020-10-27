b785542812029d560b1c6b243114c33902ed9c5c
Wessun <youssef1.zakraoui@epitech.eu>
2020-10-27 17:25:59 +0100

    Update database

--- back_end/apiTimeManager/config/dev.exs
+++ back_end/apiTimeManager/config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :apiTime, Todolist.Repo,
   username: "postgres",
   password: "postgres",
-  database: "apitime_dev",
+  database: "time_manager",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10

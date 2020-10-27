e1856edf18fef2744824be5637231e71886edf18
MileKat <arthur.barthelme78@gmail.com>
2020-10-27 12:56:44 +0100

    - Ajout router profil - AJout router equipe

--- front_end/time_manager/src/router/index.js
+++ front_end/time_manager/src/router/index.js
@@ -1,17 +1,32 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/Home";
+import Profil from "@/components/Profil";
+import Team from "@/components/Team";
 
 //  Activation du router
 Vue.use(Router);
 
 export default new Router({
+    //  Permet de retirer le /#/
+    mode: "history",
+    //  Liste des routes avec leur path
     routes: [
         {
             path: "/",
             name: "Home",
-            components: Home,
-            props: false
+            component: Home,
+            props: false,
+            children: [
+                {
+                    path: "profil",
+                    component: Profil
+                },
+                {
+                    path: "equipe",
+                    component: Team
+                }
+            ]
         }
     ]
 });

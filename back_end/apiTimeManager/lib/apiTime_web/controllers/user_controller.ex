af8eaa9e80b6b39de54d8f18e098194eed2c47fe
Wessun <youssef1.zakraoui@epitech.eu>
2020-11-02 17:14:37 +0100

    Edit status for account created

--- back_end/apiTimeManager/lib/apiTime_web/controllers/user_controller.ex
+++ back_end/apiTimeManager/lib/apiTime_web/controllers/user_controller.ex
@@ -21,7 +21,8 @@ defmodule TodolistWeb.UserController do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.user_path(conn, :show, user))
-      |> render(json(conn, %{"200": "Account created"}))
+      |> resp(200, "Account created")
+      |> send_resp()
     end
   end
 

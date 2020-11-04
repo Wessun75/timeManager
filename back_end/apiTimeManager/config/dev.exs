f5a32b5ac4c6a3a1eb711356506d5ea6bdce6a17
Wessun <youssef1.zakraoui@epitech.eu>
2020-11-04 18:19:03 +0100

    init config postgre

--- back_end/apiTimeManager/config/dev.exs
+++ back_end/apiTimeManager/config/dev.exs
@@ -3,8 +3,8 @@ use Mix.Config
 # Configure your database
 config :apiTime, Todolist.Repo,
   username: "postgres",
-  password: "ff2!26>P",
-  database: "apitime_dev",
+  password: "postgres",
+  database: "time_manager",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
